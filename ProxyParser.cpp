13974b3daa38ce3f7a1748f31ff2fd942428f179
Prathamesh <devpsk03@gmail.com>
2024-06-14 16:52:43 +0530

    created parsedHeaderCreate,parsedHeaderLineLength,parsedHeadersLength,parsedHeaderPrintHeaders updated parsedHeaderRemove function and made some changes to pass all strings by reference

--- ProxyParser.cpp
+++ ProxyParser.cpp
@@ -17,7 +17,7 @@ static void resizeHeader(ParsedHeader *&headers, size_t currentSize, size_t newS
   headers = newHeaders;
 }
 
-int parsedRequestPrintRequestLine(ParsedRequest *request, std::string buffer, size_t bufferLength, size_t *temp);
+int parsedRequestPrintRequestLine(ParsedRequest *request, std::string &buffer, size_t bufferLength, size_t *temp);
 
 size_t parsedRequestLineLength(ParsedRequest *request);
 
@@ -38,7 +38,16 @@ void debugTemplate(std::ostream &os, T first, Args... args)
   debugTemplate(os, args...);
 }
 
-int parsedHeaderSet(ParsedRequest *request, std::string key, std::string value)
+template <typename... Args>
+void debug(Args... args)
+{
+  if (DEBUG)
+  {
+    debugTemplate(std::cout, args...);
+  }
+}
+
+int parsedHeaderSet(ParsedRequest *request, std::string &key, std::string &value)
 {
   ParsedHeader *newHeader;
   parsedHeaderRemove(request, key);
@@ -54,6 +63,7 @@ int parsedHeaderSet(ParsedRequest *request, std::string key, std::string value)
     return -1;
   }
   newHeader = &(request->headers[request->headersUsed]);
+  request->headersUsed += 1;
   newHeader->key = key;
   newHeader->value = value;
   newHeader->keyLength = key.size();
@@ -62,7 +72,7 @@ int parsedHeaderSet(ParsedRequest *request, std::string key, std::string value)
   return 0;
 }
 
-ParsedHeader *parsedHeaderGet(ParsedRequest *request, std::string key)
+ParsedHeader *parsedHeaderGet(ParsedRequest *request, std::string &key)
 {
   size_t i = 0;
   ParsedHeader *temp;
@@ -78,14 +88,75 @@ ParsedHeader *parsedHeaderGet(ParsedRequest *request, std::string key)
   return nullptr;
 }
 
-int parsedHeaderRemove(ParsedRequest *request, std::string key)
+int parsedHeaderRemove(ParsedRequest *request, std::string &key)
 {
   ParsedHeader *temp = parsedHeaderGet(request, key);
   if (temp == nullptr)
   {
     return -1;
   }
-  delete temp;
+
+  temp->key.clear();
+  temp->value.clear();
+  temp->keyLength = 0;
+  temp->valueLength = 0;
+
   return 0;
 }
 
+void parsedHeaderCreate(ParsedRequest *request)
+{
+  request->headers = new ParsedHeader[DEFAULT_NO_HEADERS];
+  request->headersLength = DEFAULT_NO_HEADERS;
+  request->headersUsed = 0;
+}
+
+size_t parsedHeaderLineLength(ParsedHeader *header)
+{
+  if (header->key.size() > 0)
+  {
+    return (header->key.size() + header->value.size() + 4);
+  }
+  return 0;
+}
+
+size_t parsedHeadersLength(ParsedRequest *request)
+{
+  if (request == nullptr || request->buffer.size() == 0)
+  {
+    return 0;
+  }
+  size_t i = 0;
+  int length = 0;
+  while (i < request->headersUsed)
+  {
+    length += parsedHeaderLineLength(&(request->headers[i]));
+    i++;
+  }
+  length += 2;
+  return length;
+}
+
+int parsedHeaderPrintHeaders(ParsedRequest *request, std::string &buffer, size_t length)
+{
+  if (length < parsedHeadersLength(request))
+  {
+    debug("Buffer for printing headers is too small\n");
+    return -1;
+  }
+  ParsedHeader *header;
+  size_t i = 0;
+  while (i < request->headersUsed)
+  {
+    header = &(request->headers[i]);
+    if (header->key.size() > 0)
+    {
+      buffer += header->key;
+      buffer += ": ";
+      buffer += header->value;
+      buffer += "\r\n";
+    }
+  }
+  buffer += "\r\n";
+  return 0;
+}
--- ProxyParser.h
+++ ProxyParser.h
@@ -5,7 +5,7 @@
 #include <utility>
 #include <cerrno>
 #include <cctype>
-#include<cstring>
+#include <cstring>
 
 #define DEBUG 1
 
@@ -35,22 +35,22 @@ public:
 
 ParsedRequest *parsedRequestCreate();
 
-int parsedRequestParse(ParsedRequest *parse, std::string buffer, size_t bufferLength);
+int parsedRequestParse(ParsedRequest *parse, std::string &buffer, size_t bufferLength);
 
 void parsedRequestDestroy(ParsedRequest *request);
 
-int parsedRequestUnparse(ParsedRequest *request, std::string buffer, size_t bufferLength);
+int parsedRequestUnparse(ParsedRequest *request, std::string &buffer, size_t bufferLength);
 
-int parsedRequestUnparseHeaders(ParsedRequest *request, std::string buffer, size_t bufferLength);
+int parsedRequestUnparseHeaders(ParsedRequest *request, std::string &buffer, size_t bufferLength);
 
 size_t parsedRequestTotalLength(ParsedRequest *request);
 
 size_t parsedHeadersLength(ParsedRequest *request);
 
-ParsedHeader *parsedHeaderGet(ParsedRequest *request, std::string key);
+ParsedHeader *parsedHeaderGet(ParsedRequest *request, std::string &key);
 
-int parsedHeaderSet(ParsedRequest *request, std::string key, std::string value);
+int parsedHeaderSet(ParsedRequest *request, std::string &key, std::string &value);
 
-int parsedHeaderRemove(ParsedRequest *request, std::string key);
+int parsedHeaderRemove(ParsedRequest *request, std::string &key);
 
-void debug(std::string format, ...);
+void debug(std::string &format, ...);
